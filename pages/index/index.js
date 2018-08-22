887620d72c46f7ee226b5e3ca4bc4ac4e25c58af
活哲 <lijunzhuozoom@sina.com>
2018-08-22 20:39:35 +0800

    ljz_update

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,53 +1,105 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    list:[]
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+
+  refreshList: function (res) {
+    // console.log("--6666666666!-res---%@-----------", res);  
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      "list": res
     })
+  },
+
+
+  fetchDataListFun: function (){
+    var that = this;
+    wx.request({
+      url: 'http://appapi2.gamersky.com/v2/AllChannelList ',
+      header: {
+        'content-type': 'application/json'
+      },
+      data: { "deviceType": "iPhone7,2", "deviceId": "58B8646F-0263-4676-82A1-216AF6D609B0", "os": "iOS", "osVersion": "11.2", "app": "GSApp", "appVersion": "4.1.5", "request": { "parentNodeId": "news", "nodeIds": "0", "pageIndex": "1", "elementsCountPerPage": "20" } },
+      method: 'POST',
+      success: function (res) {
+        console.log(res);
+        var dataSource = res.data["result"];
+        for (var i = 0; i < dataSource.length; i++) {
+          var item = dataSource[i];
+          var imageUrls = item.thumbnailURLs;
+          if (imageUrls != null) {
+            for (var a = 0; a < imageUrls.length; a++) {
+              var imageurl = imageUrls[a];
+              // console.log("----imageurl-----%s-----------", imageurl);
+              if (imageurl != null) {
+                var temp = imageurl.slice(8);
+                // console.log("---temp---%s-----------", temp);
+                imageurl = "https://images.weserv.nl/?url="+temp;
+                // console.log("--!!!!!!!-imageurl---%s-----------", imageurl);
+              }
+            }
+            item.thumbnailURLs = imageUrls;
+            //  console.log("--6666666666!-res---%@-----------", dataSource);  
+          }
+        }   
+        that.refreshList(dataSource);   
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.fetchDataListFun();
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
   }
 })
